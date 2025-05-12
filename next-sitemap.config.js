1b327893eb7a8e745829924c91e61b0e061587f6
Tokenzrey <msyarifhh@gmail.com>
2025-05-12 13:57:18 +0700

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteUrl: 'https://hmtc-its.com',
+  generateRobotsTxt: true,
+  exclude: ['/login*', '/register*', '/forgot-password*', '/change-password*'],
+  changefreq: 'daily',
+  priority: 0.7,
+};
