49c3155551f77bf90021fd0cdb6bd6ebd0151492
Tokenzrey <msyarifhh@gmail.com>
2025-05-16 03:15:07 +0700

    feat: add metadata and JsonLd

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,15 +1,27 @@
 import { Metadata } from 'next';
 
 import GalleryPage from '@/app/gallery/container/galleryPage';
+import JsonLd from '@/components/JsonLd';
 
 export const metadata: Metadata = {
-  title: { default: 'Gallery HMTC', template: '%s • HMTC ITS' },
+  title: {
+    default: 'Galeri Foto & Dokumentasi | HMTC ITS',
+    template: '%s | HMTC ITS',
+  },
   description:
-    'Kumpulan foto dan momen HMTC ITS - dari workshop hingga gathering.',
+    'Kumpulan foto, video, dan dokumentasi kegiatan HMTC ITS - workshop, gathering, dan acara mahasiswa teknik komputer Institut Teknologi Sepuluh Nopember.',
   alternates: { canonical: 'https://hmtc-its.com/gallery' },
   robots: { index: true, follow: true },
 };
 
 export default function Page() {
-  return <GalleryPage />;
+  return (
+    <>
+      <JsonLd
+        title='Gallery HMTC ITS'
+        description='Kumpulan foto dan momen HMTC ITS - dari workshop hingga gathering.'
+      />
+      <GalleryPage />
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { Metadata } from 'next';
 
 import LandingPage from '@/app/landing/LandingPage';
+import JsonLd from '@/components/JsonLd';
 import Footer from '@/layouts/Footer';
 import Navbar from '@/layouts/Navbar';
 
 export const metadata: Metadata = {
-  title: { default: 'Beranda', template: '%s • HMTC ITS' },
+  title: {
+    default: 'HMTC ITS | Organisasi Mahasiswa Teknik Komputer ITS',
+    template: '%s | HMTC ITS',
+  },
   description:
-    'Selamat datang di Himpunan Mahasiswa Teknik Komputer (HMTC) ITS — workshop, galeri, blog, dan komunitas mahasiswa.',
+    'Selamat datang di Himpunan Mahasiswa Teknik Komputer (HMTC) ITS — workshop, galeri, blog, dan komunitas mahasiswa teknik komputer terkemuka.',
   alternates: { canonical: 'https://hmtc-its.com' },
   robots: { index: true, follow: true },
 };
@@ -15,6 +19,10 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
+      <JsonLd
+        title='HMTC ITS'
+        description='Selamat datang di Himpunan Mahasiswa Teknik Komputer (HMTC) ITS — workshop, galeri, blog, dan komunitas mahasiswa teknik komputer terkemuka.'
+      />
       <Navbar />
       <LandingPage />
       <Footer />
